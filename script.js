040b096fd31deb03c6e464d28a96998928c2e192
andres0818 <37464506+andres0818@users.noreply.github.com>
2022-12-01 15:50:07 -0500

    primera subida

---
+++ script.js
@@ -0,0 +1,52 @@
+
+
+function usuario() {
+
+    let targetas=document.querySelector(".container-html")
+        targetas.setAttribute("style","display: none;")
+    let btnComprar=document.querySelector(".container-css")
+        btnComprar.setAttribute("style","display:none ;")
+    let ventaForm=document.querySelector(".container")
+        ventaForm.setAttribute("style","display:block ;")
+    let formDatosVenta=document.querySelector(".container-js")
+        formDatosVenta.setAttribute("style","display:none ;")
+             
+    }
+
+
+function html() {
+
+    let targetas=document.querySelector(".container-html")
+        targetas.setAttribute("style","display: block;")
+    let btnComprar=document.querySelector(".container-css")
+        btnComprar.setAttribute("style","display:none ;")
+    let ventaForm=document.querySelector(".container")
+        ventaForm.setAttribute("style","display:none ;")
+    let formDatosVenta=document.querySelector(".container-js")
+        formDatosVenta.setAttribute("style","display:none ;")
+     
+    }
+    function css() {
+
+    let targetas=document.querySelector(".container-html")
+        targetas.setAttribute("style","display: none;")
+    let btnComprar=document.querySelector(".container-css")
+        btnComprar.setAttribute("style","display:block ;")
+    let ventaForm=document.querySelector(".container")
+        ventaForm.setAttribute("style","display:none ;")
+    let formDatosVenta=document.querySelector(".container-js")
+        formDatosVenta.setAttribute("style","display:none ;")
+         
+    }
+    function js() {
+
+    let targetas=document.querySelector(".container-html")
+        targetas.setAttribute("style","display: none;")
+    let btnComprar=document.querySelector(".container-css")
+        btnComprar.setAttribute("style","display:none ;")
+    let ventaForm=document.querySelector(".container")
+        ventaForm.setAttribute("style","display:none ;")
+    let formDatosVenta=document.querySelector(".container-js")
+        formDatosVenta.setAttribute("style","display:block ;")
+             
+    }
